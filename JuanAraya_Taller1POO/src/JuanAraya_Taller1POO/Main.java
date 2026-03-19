8cf7d6f5a4044172037686428a0a1183cc7d2634
Juan Araya Larraguibel <juan.araya02@alumnos.ucn.cl>
2026-03-19 16:54:51 -0300

    Creacion de los vectores
    se agregaron los vectores para poder añadir los elementos a listas con un maximo para no sobreexplotar todo

--- JuanAraya_Taller1POO/src/JuanAraya_Taller1POO/Main.java
+++ JuanAraya_Taller1POO/src/JuanAraya_Taller1POO/Main.java
@@ -1,10 +1,23 @@
 package JuanAraya_Taller1POO;
-
+//Nombre Integrante: Juan Ignacio Araya Larraguibel
 public class Main {
-
+	//uso de vectores para tener tamaño fijo en usuarios y registros
+	public static int usuariosMaximos= 3;
+	public static int registrosMaximos= 300;
+	
+	public static String[] usuarios = new String[usuariosMaximos];
+	public static String[] contraseñas = new String[usuariosMaximos];
+	public static int usuariosTotales= 0;
+	
+	public static String[] registroUsuario = new String[registrosMaximos];
+	public static String[] registroFecha = new String[registrosMaximos];
+	public static int[] registroHoras = new int[registrosMaximos];
+	public static String[] registroActividad = new String[registrosMaximos];
+	public static int totalRegistros = 0;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//main creado por defecto en la clase
+		
 	}
 
 }
