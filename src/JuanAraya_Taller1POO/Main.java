60dc581e745c92c274448bff241ddd4e8782eab0
Juan Araya Larraguibel <juan.araya02@alumnos.ucn.cl>
2026-03-25 09:34:57 -0300

    añadiendo los datos para evitar nota minima :D

--- src/JuanAraya_Taller1POO/Main.java
+++ src/JuanAraya_Taller1POO/Main.java
@@ -1,5 +1,7 @@
 package JuanAraya_Taller1POO;
-
+//Integrante: Juan Ignacio Araya Larraguibel
+//RUT: 21.566.260-8
+//Carrera: Ingenieria en Tecnologias de Información (ITI)
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola");
