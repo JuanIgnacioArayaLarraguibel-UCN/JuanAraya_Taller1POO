23feaccf2d1988cd092f048cc49fa4eb8a6f7722
Juan Araya Larraguibel <juan.araya02@alumnos.ucn.cl>
2026-03-26 21:33:21 -0300

    Abriendo Scanner y arreglos(listas)
    abriendo el scanner y agregando las listas para los usuarios y registros

--- src/JuanAraya_Taller1POO/Main.java
+++ src/JuanAraya_Taller1POO/Main.java
@@ -2,9 +2,31 @@ package JuanAraya_Taller1POO;
 //Integrante: Juan Ignacio Araya Larraguibel
 //RUT: 21.566.260-8
 //Carrera: Ingenieria en Tecnologias de Información (ITI)
+
+import java.util.Scanner;
+
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		
+		//apertura del Scanner y las constantes para el maximo
+		Scanner scanner = new Scanner(System.in);
+		int usuariosMaximo= 10;
+		int registrosMaximos= 300;
+		//listas para los usuarios
+		String[] usuarioNombres= new String[usuariosMaximo];
+		String[] contraseñaUsuarios= new String[usuariosMaximo];
+		int usuariosTotales=0;
+		
+		//listas para los registros
+		String[] registroUsuario= new String[registrosMaximos];
+		String[] registroFecha= new String[registrosMaximos];
+		int[] registroHoras= new int[registrosMaximos];
+		String[] registroActividad= new String[registrosMaximos];
+		int registrosTotales=0;
+		
+		
+		
 	}
 
 }
