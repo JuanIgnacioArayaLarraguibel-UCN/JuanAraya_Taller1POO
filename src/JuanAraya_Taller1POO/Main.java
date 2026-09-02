50a297b3f2c9717aac7ac5e8d47fbcce850e0c00
Juan Araya Larraguibel <juan.araya02@alumnos.ucn.cl>
2026-09-02 14:40:38 -0400

    adición de los TXT  y reponiendo datos personales

--- src/JuanAraya_Taller1POO/Main.java
+++ src/JuanAraya_Taller1POO/Main.java
@@ -1,7 +1,5 @@
 package JuanAraya_Taller1POO;
-//Nombre: Juan Ignacio Araya Larraguibel
-//RUT: 21.566.260-8
-//Carrera: Ingenieria en Tecnologias de Información
+//Juan Ignacio Araya Larraguibel - 21.566.260-8 - ITI
 
 
 public class Main {
