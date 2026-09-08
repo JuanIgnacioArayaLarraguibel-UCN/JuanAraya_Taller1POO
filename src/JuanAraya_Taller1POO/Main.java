8481ec5ded95b95a1f54120127a8e44f6b132ece
Juan Araya Larraguibel <juan.araya02@alumnos.ucn.cl>
2026-09-07 22:11:42 -0300

    Creando la capacidad maxima para los vectores

--- src/JuanAraya_Taller1POO/Main.java
+++ src/JuanAraya_Taller1POO/Main.java
@@ -1,11 +1,19 @@
 package JuanAraya_Taller1POO;
 //Juan Ignacio Araya Larraguibel - 21.566.260-8 - ITI
 
-
 public class Main {
+	
+	//Esto es la capacidad maxima para los vectores
+	private static int alumnosMaximos =100;
+	private static int solicitudesMaximas =100;
+	private static int admitidosMaximos =100;
+	private static int rechazadosMaximos =100;
+	
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
 
