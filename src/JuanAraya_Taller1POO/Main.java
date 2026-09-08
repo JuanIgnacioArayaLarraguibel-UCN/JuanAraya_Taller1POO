eaec8cafe1eff2d5b18518094ab32f962f6e6f40
Juan Araya Larraguibel <juan.araya02@alumnos.ucn.cl>
2026-09-07 22:29:35 -0300

    añadiendo los Vectores

--- src/JuanAraya_Taller1POO/Main.java
+++ src/JuanAraya_Taller1POO/Main.java
@@ -9,6 +9,25 @@ public class Main {
 	public static int admitidosMaximos =100;
 	public static int rechazadosMaximos =100;
 	
+	//vectores :P
+	public static String[] nombreAlumnos= new String[alumnosMaximos];
+	public static String[] apellidoAlumnos= new String[alumnosMaximos];
+	public static String[] rutAlumnos= new String[alumnosMaximos];
+	public static String[] paraleloAlumnos= new String[alumnosMaximos];
+	public static int alumnosTotales=0;
+	public static String[] nombreSolicitudes= new String[solicitudesMaximas];
+	public static String[] apellidoSolicitudes= new String[solicitudesMaximas];
+	public static int solicitudesTotales=0;
+	public static String[] nombresAdmitidos= new String[admitidosMaximos];
+	public static String[] apellidosAdmitidos= new String[admitidosMaximos];
+	public static String[] rutsAdmitidos= new String[admitidosMaximos];
+	public static String[] paralelosAdmitidos= new String[admitidosMaximos];
+	public static int admitidosTotales=0;
+	public static String[] nombresRechazados= new String[rechazadosMaximos];
+	public static String[] apellidosRechazados= new String[rechazadosMaximos];
+	public static String[] rutsRechazados= new String[rechazadosMaximos];
+	public static int rechazadosTotales=0;
+	
 	
 
 	public static void main(String[] args) {
